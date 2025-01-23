3487c07abfeeea0e11c0ca719075185eaaba4ecd
Martro96 <martarodrigo.55@gmail.com>
2025-01-23 12:30:09 +0100

    Avanzo con routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importo esto para usar *ngIf
-import { ArticleListComponent } from './components/article-list/article-list.component';
+import { CommonModule } from '@angular/common'; 
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ArticleNewTemplateComponent } from './components/article-new-template/article-new-template.component'; 
-import { ArticleNewReactiveComponent } from './components/article-new-reactive/article-new-reactive.component';
+import { RouterModule } from '@angular/router';
+//import { ArticleListComponent } from './components/article-list/article-list.component';
+//import { ArticleNewTemplateComponent } from './components/article-new-template/article-new-template.component'; 
+//import { ArticleNewReactiveComponent } from './components/article-new-reactive/article-new-reactive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   imports: [
-    CommonModule, // Importo esto para usar *ngIf
-    ArticleListComponent, 
-    NavbarComponent, 
-    ArticleNewTemplateComponent, 
-    ArticleNewReactiveComponent],
+    CommonModule, 
+    NavbarComponent,
+    RouterModule,
+    // ArticleListComponent, 
+    // ArticleNewTemplateComponent, 
+    // ArticleNewReactiveComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecommerce';
 
-  // Estado para gestionar la vista actual
-  currentView: string = 'articles'; // Vista por defecto
-
-  // Método para cambiar la vista
-  changeView(view: string) {
-    this.currentView = view;
-  }
+  // currentView: string = 'articles'; Eliminamos esto porque ahora trabajamos por routing
+  // changeView(view: string) {
+  //   this.currentView = view;
+  // }
 
 }
