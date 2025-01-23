a251618196eb0377d8d2128d1e8341305a264916
Martro96 <martarodrigo.55@gmail.com>
2025-01-23 12:46:08 +0100

    Hago pruebas con el routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter, Routes } from '@angular/router';
+// import { routes } from './app.routes';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { provideHttpClient, withFetch  } from '@angular/common/http';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch  } from '@angular/common/http';
 
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideHttpClient(withFetch()), 
+//     provideZoneChangeDetection({ eventCoalescing: true }), 
+//     // provideRouter(routes), 
+//     provideClientHydration(withEventReplay())],
+
+// };
+import { ApplicationConfig, provideRouter } from '@angular/core';
+import { routes } from './app.routes'; // Aquí importas las rutas
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()), 
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideClientHydration(withEventReplay())],
-
+    provideRouter(routes), // Usas provideRouter para configurar las rutas
+  ],
 };
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,47 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Route } from "@angular/router";
+// import { NgModule } from "@angular/core";
+// import { RouterModule, Routes } from "@angular/router";
+// import { LoginComponent } from "./components/login/login.component";
+// import { RegisterComponent } from "./components/register/register.component";
+// import { ArticleListComponent } from "./components/article-list/article-list.component";
+// import { ArticleNewReactiveComponent } from "./components/article-new-reactive/article-new-reactive.component";
+// import { ArticleDetailComponent } from "./components/article-detail/article-detail.component";
+// import { provideHttpClient, withInterceptors } from "@angular/common/http";
+// import { articleAppInterceptor } from "./interceptors/article-app.interceptor";
+
+// export const routes: Routes = [ 
+//   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { path: 'article/list', component: ArticleListComponent },
+//   { path: 'article/create', component: ArticleNewReactiveComponent }, 
+//   { path: 'article/:id', component: ArticleDetailComponent }, 
+// ];
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {}
+
+
+// // export const appConfig = [
+// //   provideRouter(routes),
+// //   provideHttpClient(withInterceptors([articleAppInterceptor])),
+// // ];
+
+import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { ArticleListComponent } from "./components/article-list/article-list.component";
 import { ArticleNewReactiveComponent } from "./components/article-new-reactive/article-new-reactive.component";
-import { provideRouter, Routes } from "@angular/router";
 import { ArticleDetailComponent } from "./components/article-detail/article-detail.component";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
-import { articleAppInterceptor } from "./interceptors/article-app.interceptor";
 
-export const routes: Routes = [ 
+export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'article/list', component: ArticleListComponent },
   { path: 'article/create', component: ArticleNewReactiveComponent }, 
   { path: 'article/:id', component: ArticleDetailComponent }, 
-];
-
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-
-
-export const appConfig = [
-  provideRouter(routes),
-  provideHttpClient(withInterceptors([articleAppInterceptor])),
 ];
