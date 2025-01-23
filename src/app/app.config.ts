66693e4bbb3d535a6124823484198ec26cb35bdc
Martro96 <martarodrigo.55@gmail.com>
2025-01-23 12:57:43 +0100

    Actualizo Angular, tengo problemas con el routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,15 @@
-// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 // import { provideRouter, Routes } from '@angular/router';
 // import { routes } from './app.routes';
-// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-// import { provideHttpClient, withFetch  } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch  } from '@angular/common/http';
 
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideHttpClient(withFetch()), 
-//     provideZoneChangeDetection({ eventCoalescing: true }), 
-//     // provideRouter(routes), 
-//     provideClientHydration(withEventReplay())],
-
-// };
-import { ApplicationConfig, provideRouter } from '@angular/core';
-import { routes } from './app.routes'; // Aquí importas las rutas
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), // Usas provideRouter para configurar las rutas
-  ],
-};
+    provideHttpClient(withFetch()), 
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    // provideRouter(routes), 
+    provideClientHydration(withEventReplay())],
 
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,14 @@
-// import { NgModule } from "@angular/core";
-// import { RouterModule, Routes } from "@angular/router";
-// import { LoginComponent } from "./components/login/login.component";
-// import { RegisterComponent } from "./components/register/register.component";
-// import { ArticleListComponent } from "./components/article-list/article-list.component";
-// import { ArticleNewReactiveComponent } from "./components/article-new-reactive/article-new-reactive.component";
-// import { ArticleDetailComponent } from "./components/article-detail/article-detail.component";
-// import { provideHttpClient, withInterceptors } from "@angular/common/http";
-// import { articleAppInterceptor } from "./interceptors/article-app.interceptor";
-
-// export const routes: Routes = [ 
-//   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: RegisterComponent },
-//   { path: 'article/list', component: ArticleListComponent },
-//   { path: 'article/create', component: ArticleNewReactiveComponent }, 
-//   { path: 'article/:id', component: ArticleDetailComponent }, 
-// ];
-
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule {}
-
-
-// // export const appConfig = [
-// //   provideRouter(routes),
-// //   provideHttpClient(withInterceptors([articleAppInterceptor])),
-// // ];
-
-import { Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { ArticleListComponent } from "./components/article-list/article-list.component";
 import { ArticleNewReactiveComponent } from "./components/article-new-reactive/article-new-reactive.component";
 import { ArticleDetailComponent } from "./components/article-detail/article-detail.component";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { articleAppInterceptor } from "./interceptors/article-app.interceptor";
 
-export const routes: Routes = [
+export const routes: Routes = [ 
   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -45,3 +16,11 @@ export const routes: Routes = [
   { path: 'article/create', component: ArticleNewReactiveComponent }, 
   { path: 'article/:id', component: ArticleDetailComponent }, 
 ];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
