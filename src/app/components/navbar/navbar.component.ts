45629f263781c7029438edfbbf48d950ee658f11
Martro96 <martarodrigo.55@gmail.com>
2025-01-23 23:51:39 +0100

    Corrijo error navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, } from '@angular/core'; //Quito EventEmitter y Output porque usamos routerLink
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  //Elimino @Output() y EventEmitter porque usamos routerLink
-  // @Output() viewChange = new EventEmitter<string>(); 
-  // changeView(view: string) {
-  //   this.viewChange.emit(view);
-  // }
+  isMenuCollapsed = true;
+
+  toggleMenu(){
+    this.isMenuCollapsed =!this.isMenuCollapsed;
+  }
 }
